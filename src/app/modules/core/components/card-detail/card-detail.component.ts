66c0b35dbea00b05913e9dc890e5cd89976a340d
Chiara Grottini <chiaragrottini@gmail.com>
2021-11-16 14:07:06 +0100

    fixed cart function

--- src/app/modules/core/components/card-detail/card-detail.component.ts
+++ src/app/modules/core/components/card-detail/card-detail.component.ts
@@ -4,19 +4,19 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-card-detail',
   templateUrl: './card-detail.component.html',
-  styleUrls: ['./card-detail.component.css']
+  styleUrls: ['./card-detail.component.css'],
 })
 export class CardDetailComponent implements OnInit {
-  @Output() closedDetail = new EventEmitter();
   @Input() product!: IProduct;
 
-  constructor() { }
+  @Output() addToCart = new EventEmitter<IProduct>();
+  @Output() closedDetail = new EventEmitter();
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
-  closeDetail(){
+  ngOnInit(): void {}
+
+  closeDetail() {
     this.closedDetail.emit();
   }
-
 }
